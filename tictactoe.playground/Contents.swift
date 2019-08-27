57dd6356a82a5d34927d9372cafae406ebe35ce4
1 <a1@1s-Mac-mini.local>
2019-08-27 14:49:50 +0400

    Correct checkEnd() function

--- tictactoe.playground/Contents.swift
+++ tictactoe.playground/Contents.swift
@@ -1,5 +1,27 @@
-import Foundation
-import UIKit
+protocol Game {
+    var name: String { get }
+    var state: String { get }
+}
+
+protocol GameTracking {
+    func gameDidStart(_ game: Game)
+    func gameDidUpdateState(_ game: Game)
+    func gameDidEnd(_ game: Game)
+}
+
+struct Tracker: GameTracking {
+    func gameDidStart(_ game: Game) {
+        print("\(game.name) did start!")
+    }
+    
+    func gameDidUpdateState(_ game: Game) {
+        print("Game did update:\n\(game.state)")
+    }
+    
+    func gameDidEnd(_ game: Game) {
+        print("\(game.name) did end")
+    }
+}
 
 enum BoardType: Int {
     case small = 3
@@ -7,89 +29,42 @@ enum BoardType: Int {
     case large = 7
 }
 
+struct MatrixIndex: Hashable {
+    let row, column: Int
+    
+    init(_ row: Int, _ column: Int) {
+        self.row = row
+        self.column = column
+    }
+    
+    func onMainDiagonal() -> Bool {
+        return row == column
+    }
+    
+    func onCounterDiagonal(_ matrixSize: Int) -> Bool {
+        return row == matrixSize - 1 - column
+    }
+}
+
 struct Matrix {
+    
     let boardSize: Int
     let boardType: BoardType
     var board: [Character]
-    var isXWinner = false
-    var haveWinner = false
     
-    init(boardType: BoardType) {
-        self.boardType = boardType
-        boardSize = self.boardType.rawValue
+    init(_ type: BoardType) {
+        self.boardType = type
+        boardSize = type.rawValue
         board = Array(repeating: "_", count: boardSize * boardSize)
     }
     
-    struct MatrixIndex: Hashable {
-        let row, column: Int
-        
-        init(_ row: Int, _ column: Int) {
-            self.row = row
-            self.column = column
+    subscript(index: MatrixIndex) -> Character {
+        set {
+            board[index.row * boardSize + index.column] = newValue
         }
-    }
-    
-    mutating func stepPlayerX(_ matrIdx: MatrixIndex) {
-        print("")
-        print("Step of player X")
         
-        board[matrIdx.row * boardSize + matrIdx.column] = "x"
-    }
-    
-    mutating func stepPlayerO(_ matrIdx: MatrixIndex) {
-        print("")
-        print("Step of player O")
-        
-        board[matrIdx.row * boardSize + matrIdx.column] = "o"
-    }
-    
-    func currentBoardState() {
-        for i in 0 ..< boardSize * boardSize {
-            if i % boardSize == 0 {
-                print("")
-            }
-            print(board[i], terminator: " ")
-        }
-        print("")
-    }
-
-    mutating func checkEnd() -> Bool {
-        switch boardType {
-        case .small:
-            for i in 0 ..< boardSize {
-                if (board[i] == board[i + 1]) && (board[i + 1] == board[i + 2]) && (board[i + 2] != "_") {
-                    if board[i] == "x" {
-                        isXWinner = true
-                    }
-                    haveWinner = true
-                    return haveWinner
-                } else if (board[i] == board[i + boardSize]) && (board[i + boardSize] == board[i + 2 * boardSize]) && (board[i + 2 * boardSize] != "_") {
-                    if board[i] == "x" {
-                        isXWinner = true
-                    }
-                    haveWinner = true
-                    return haveWinner
-                } else if (i == 0 && (board[0] == board[4]) && (board[4] == board[8]) && (board[8] != "_")) {
-                    if board[i] == "x" {
-                        isXWinner = true
-                    }
-                    haveWinner = true
-                    return haveWinner
-                } else if (i == 2 && (board[2] == board[4]) && (board[4] == board[6]) && (board[6] != "_")) {
-                    if board[i] == "x" {
-                        isXWinner = true
-                    }
-                    haveWinner = true
-                    return haveWinner
-                }
-            }
-            return haveWinner
-        case .middle:
-            print("No information about winner.")
-            return false
-        case .large:
-            print("No information about winner.")
-            return false
+        get {
+            return board[index.row * boardSize + index.column]
         }
     }
     
@@ -104,38 +79,157 @@ struct Matrix {
     }
 }
 
-var game = Matrix(boardType: BoardType.small)
-print("Start game")
+struct TicTacToe {
+    var matrix: Matrix
+    var isXWinner: Bool?
+    var lastStep: MatrixIndex? = nil
+    
+    var tracker: GameTracking?
+    
+    init(_ boardType: BoardType, _ tracker: GameTracking? = nil) {
+        matrix = Matrix(boardType)
+        self.tracker = tracker
+        self.tracker?.gameDidStart(self)
+    }
+    
+    mutating func stepPlayerX(_ matrIdx: MatrixIndex) {
+        print("\nStep of player X")
+        matrix[matrIdx] = "x"
+        lastStep = matrIdx
+        tracker?.gameDidUpdateState(self)
+    }
+    
+    mutating func stepPlayerO(_ matrIdx: MatrixIndex) {
+        print("Step of player O")
+        matrix[matrIdx] = "o"
+        lastStep = matrIdx
+        tracker?.gameDidUpdateState(self)
+    }
+    
+    func checkRowOnEqualValues(_ step: MatrixIndex) -> Bool {
+        for colInd in 0 ..< matrix.boardSize - 1 {
+            if matrix[MatrixIndex(step.row, colInd)] != matrix[MatrixIndex(step.row, colInd + 1)] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func checkColumnOnEqualValues(_ step: MatrixIndex) -> Bool {
+        for rowInd in 0 ..< matrix.boardSize - 1 {
+            if matrix[MatrixIndex(rowInd, step.column)] != matrix[MatrixIndex(rowInd + 1, step.column)] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func CheckMainDiagonalOnEqualValues() -> Bool {
+        for idx in 0 ..< matrix.boardSize - 1 {
+            if matrix[MatrixIndex(idx, idx)] != matrix[MatrixIndex(idx + 1, idx + 1)] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func CheckCounterDiagonalOnEqualValues() -> Bool {
+        for idx in 0 ..< matrix.boardSize - 1 {
+            if (matrix[MatrixIndex(idx, matrix.boardSize - 1 - idx)] != matrix[MatrixIndex(idx + 1, matrix.boardSize - 2 - idx)]) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    mutating func checkEnd() -> Bool {
+        guard let step = lastStep else {
+            return false
+        }
+        
+        var isEnd = checkRowOnEqualValues(step)
+        
+        if !isEnd {
+            isEnd = checkColumnOnEqualValues(step)
+        } else {
+            isXWinner = (matrix[step] == "x") ? true : false
+            return true
+        }
+        
+        if isEnd {
+            isXWinner = (matrix[step] == "x") ? true : false
+            return true
+        }
+        
+        // check step situation on diagonal or counter-diagonal
+        let onMainDiagonal = step.onMainDiagonal()
+        let onCounterDiagonal = step.onCounterDiagonal(matrix.boardSize)
+        
+        if !onMainDiagonal || !onCounterDiagonal {
+            return isEnd
+        }
+        
+        if onMainDiagonal {
+            isEnd = CheckMainDiagonalOnEqualValues()
+        }
+        
+        if isEnd {
+            isXWinner = (matrix[step] == "x") ? true : false
+            return true
+        }
+        
+        if onCounterDiagonal {
+            isEnd = CheckCounterDiagonalOnEqualValues()
+        }
+        
+        if isEnd {
+            isXWinner = (matrix[step] == "x") ? true : false
+            return true
+        }
+        
+        return isEnd
+    }
+}
 
-var indices = game.indices
+extension TicTacToe: Game {
+    var name: String {
+        return "TicTacToe"
+    }
+    
+    var state: String {
+        var str = ""
+        for i in 0 ..< matrix.board.count {
+            if i % matrix.boardSize == 0 {
+                str += "\n"
+            }
+            str += (String(matrix.board[i]) + " ")
+        }
+        return str
+    }
+}
+
+var game = TicTacToe(BoardType.middle, Tracker())
+
+var indices = game.matrix.indices
+
 var isXTurn = true
-
-game.currentBoardState()
-//while let idx = indices.randomElement() {
-//    isXTurn ? game.stepPlayerX(idx) : game.stepPlayerO(idx)
-//    indices.remove(idx)
-//    isXTurn = !isXTurn
-//    game.currentBoardState()
-//}
 
 while !game.checkEnd() && !indices.isEmpty {
     let idx = indices.randomElement()
     isXTurn ? game.stepPlayerX(idx!) : game.stepPlayerO(idx!)
     indices.remove(idx!)
+    print("Value: \(game.matrix[idx!])")
     isXTurn = !isXTurn
-    game.currentBoardState()
 }
 
-if game.haveWinner {
-    if game.isXWinner {
-        print("The winner is X.")
-    } else {
-        print("The winner is O.")
-    }
-} else {
+switch(game.isXWinner) {
+case true:
+    print("The winner is X")
+case false:
+    print("The winner is O")
+case nil:
     print("No winner")
+default:
+    print("Invalid case")
 }
-
-print("\nEnd game")
-
 
